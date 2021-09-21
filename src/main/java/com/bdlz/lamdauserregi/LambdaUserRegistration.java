3f3c28d2b6f8c35963ecbf2d40bd7c03386184a9
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-22 00:33:30 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/lamdauserregi/LambdaUserRegistration.java
@@ -0,0 +1,7 @@
+package com.bdlz.lamdauserregi;
+
+public class LambdaUserRegistration {
+    public static void main(String[] args) {
+        System.out.println("welcome to the exception user Registration program");
+    }
+}
